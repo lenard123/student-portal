ae0463ea8300aaf3dc79e57be0fbcefca6a0bd24
Lenard  Mangay-ayam <lenard.mangayayam@gmail.com>
2022-06-24 16:52:32 +0000

    WIP

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 </head>
 <body>
   <?= component_start('layouts/main') ?>
-    <div class="hero py-16 bg-base-200">
+    <section id="home" class="hero py-16 bg-base-200">
       <div class="hero-content text-center">
         <div class="max-w-md">
           <h1 class="text-5xl font-bold">Hello there</h1>
@@ -15,7 +15,53 @@
           <button class="btn btn-primary">Get Started</button>
         </div>
       </div>
-    </div>
+    </section>
+
+    <section id="about">
+
+    </section>
+
+    <section id="contact" class="container py-12">
+        <div class="text-center">
+            <h1 class="text-2xl font-bold">Contact Us</h1>
+            <div>If you have a question or inquiries feel free to leave a message.</div>
+        </div>
+
+
+        <form class="mt-4 max-w-screen-sm mx-auto">
+            <div class="grid sm:grid-cols-2 sm:gap-4">
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Name: </span>
+                    </label>
+                    <input type="text" placeholder="Input your name here" class="input input-bordered w-full" />
+                </div>
+
+
+                <div class="form-control">
+                    <label class="label">
+                        <span class="label-text">Email: </span>
+                    </label>
+                    <input type="text" placeholder="Input your email here" class="input input-bordered w-full" />
+                </div>
+            </div>
+
+
+            <div class="form-control">
+                <label class="label">
+                    <span class="label-text">Message: </span>
+                </label>
+                <textarea type="text" placeholder="Write something" class="textarea textarea-bordered w-full" ></textarea>
+            </div>
+
+            <div class="form-control mt-4">
+                <button class="btn btn-primary">Send</button>
+            </div>
+
+        </form>
+
+    </section>
+
   <?= component_end() ?>
 </body>
 </html>
--- templates/layouts/main/navbar.php
+++ templates/layouts/main/navbar.php
@@ -1,10 +1,6 @@
-<header class="navbar bg-base-100 shadow-md">
+<header class="navbar bg-base-100 shadow-md sticky top-0 z-10">
 
     <div class="flex-1">
-
-        <button class="md:hidden btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path></svg>
-        </button>
 
         <a href="<?= url() ?>" class="btn btn-ghost normal-case text-xl space-x-2">
             <img src="<?= asset('img/logo.png') ?>" height="36" width="36">
@@ -12,9 +8,9 @@
         </a>
 
         <ul class="hidden md:flex menu menu-horizontal p-0">
-            <li><a href="<?= url('') ?>">Home</a></li>
-            <li><a href="<?= url('about.php') ?>">About</a></li>
-            <li><a href="<?= url('contact.php') ?>">Contact</a></li>
+            <li><a href="#home">Home</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#contact">Contact</a></li>
         </ul>
 
     </div>
@@ -25,9 +21,17 @@
             <a href="<?= url('register.php') ?>" class="btn btn-primary">Register</a>
         </div>
 
-        <button class="sm:hidden btn btn-square btn-ghost">
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
-        </button>
+        <div class="dropdown dropdown-end">
+            <label tabindex="0" class="sm:hidden btn btn-square btn-ghost">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block w-5 h-5 stroke-current"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path></svg>
+            </label>
+            <ul tabindex="0" class="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                <li><a href="<?= url('login.php') ?>">Login</a></li>
+                <li><a href="<?= url('register.php') ?>">Register</a></li>
+            </ul>
+        </div>
+
+
 
     </nav>
 

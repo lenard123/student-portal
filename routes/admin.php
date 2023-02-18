7c3b0b4b63b2c602e89c4b507aaffd093b4222ce
lenard123 <lenard.mangayayam@gmail.com>
2023-02-18 11:15:40 +0000

    Add admin routes for authentication, dashboard, settings, faculty, subjects, classes, students, announcements, and messages

---
+++ routes/admin.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Http\Controllers\Admin\AnnouncementController;
+use App\Http\Controllers\Admin\FacultyController;
+use App\Http\Controllers\Admin\MessageController;
+use App\Http\Controllers\Admin\SchoolYearController;
+use App\Http\Controllers\Admin\SectionController;
+use App\Http\Controllers\Admin\StudentController;
+use App\Http\Controllers\Admin\SubjectController;
+use App\Http\Controllers\Auth\LoginController;
+use App\Http\Controllers\SettingsController;
+use Illuminate\Support\Facades\Route;
+
+Route::view('/login', 'pages.auth.admin-login')->withoutMiddleware('auth:admin');
+Route::post('/login', [LoginController::class, 'adminLogin'])->withoutMiddleware('auth:admin');
+
+Route::view('/', 'pages.admin.dashboard');
+Route::get('/faculty', [FacultyController::class, 'index']);
+Route::get('/subjects', [SubjectController::class, 'index']);
+Route::get('/classes', [SectionController::class, 'index']);
+Route::get('/classes/new', [SectionController::class, 'createForm']);
+Route::get('/classes/{section}', [SectionController::class, 'show']);
+
+Route::get('/students', [StudentController::class, 'index']);
+Route::get('/students/{student}/edit', [StudentController::class, 'editForm']);
+Route::get('/students/{student}/enroll', [StudentController::class, 'enrollForm']);
+Route::put('/students/{student}/enroll', [StudentController::class, 'enroll']);
+Route::put('/students/{student}', [StudentController::class, 'update']);
+
+
+Route::get('/announcements', [AnnouncementController::class, 'index']);
+Route::get('/announcements/new', [AnnouncementController::class, 'showCreateForm']);
+Route::get('/announcements/{announcement}/edit', [AnnouncementController::class, 'showEditForm']);
+Route::get('/announcements/{announcement}', [AnnouncementController::class, 'show']);
+Route::put('/announcements/{announcement}', [AnnouncementController::class, 'update']);
+Route::post('/announcements', [AnnouncementController::class, 'create']);
+
+Route::get('/messages', [MessageController::class, 'index']);
+Route::get('/messages/{user}', [MessageController::class, 'show']);
+Route::post('/messages/{user}', [MessageController::class, 'create']);
+
+Route::put('/classes/{section}/subjects/{course}', [SectionController::class, 'updateSubject']);
+Route::post('/classes', [SectionController::class, 'create']);
+
+Route::get('/settings/school-year', [SchoolYearController::class, 'index']);
+Route::patch('/settings/active-department', [SettingsController::class, 'updateActiveDepartment']);
+Route::post('/settings/school-year', [SchoolYearController::class, 'create']);
+Route::patch('/settings/school-year', [SchoolYearController::class, 'updateActiveSchoolYear']);
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,10 @@
 <?php
 
-use App\Http\Controllers\Admin\AnnouncementController;
-use App\Http\Controllers\Admin\FacultyController;
-use App\Http\Controllers\Admin\MessageController;
-use App\Http\Controllers\Admin\SchoolYearController;
-use App\Http\Controllers\Admin\SectionController;
-use App\Http\Controllers\Admin\StudentController;
-use App\Http\Controllers\Admin\SubjectController;
 use App\Http\Controllers\Auth\LoginController;
 use App\Http\Controllers\Auth\RegisterController;
-use App\Http\Controllers\SettingsController;
 use App\Http\Controllers\Student\ClassesController;
 use App\Http\Controllers\Student\HomeController;
+use App\Http\Controllers\Student\MessageController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,41 +28,8 @@ Route::post('/register', RegisterController::class);
 Route::group(['middleware' => 'auth'], function () {
     Route::get('/', HomeController::class);
     Route::get('/classes', ClassesController::class);
-    Route::view('/messages', 'pages.student.messages');
+    Route::get('/messages', [MessageController::class, 'index']);
+    Route::post('/messages', [MessageController::class, 'create']);
     Route::view('/profile', 'pages.student.profile');
     Route::view('/settings', 'pages.student.settings');
-});
-
-Route::view('/admin/login', 'pages.auth.admin-login');
-Route::post('/admin/login', [LoginController::class, 'adminLogin']);
-
-Route::group(['prefix' => 'admin', 'middleware' => 'auth:admin'], function () {
-
-    Route::view('/', 'pages.admin.dashboard');
-    Route::get('/settings/school-year', [SchoolYearController::class, 'index']);
-    Route::get('/faculty', [FacultyController::class, 'index']);
-    Route::get('/subjects', [SubjectController::class, 'index']);
-    Route::get('/classes', [SectionController::class, 'index']);
-    Route::get('/classes/new', [SectionController::class, 'createForm']);
-    Route::get('/classes/{section}', [SectionController::class, 'show']);
-    Route::get('/students', [StudentController::class, 'index']);
-    Route::get('/students/{student}/edit', [StudentController::class, 'editForm']);
-    Route::get('/students/{student}/enroll', [StudentController::class, 'enrollForm']);
-    Route::get('/announcements', [AnnouncementController::class, 'index']);
-    Route::get('/announcements/new', [AnnouncementController::class, 'showCreateForm']);
-    Route::get('/announcements/{announcement}/edit', [AnnouncementController::class, 'showEditForm']);
-    Route::get('/announcements/{announcement}', [AnnouncementController::class, 'show']);
-    Route::get('/messages', [MessageController::class, 'index']);
-    Route::get('/messages/{user}', [MessageController::class, 'show']);
-
-    Route::post('/messages/{user}', [MessageController::class, 'create']);
-    Route::put('/announcements/{announcement}', [AnnouncementController::class, 'update']);
-    Route::post('/announcements', [AnnouncementController::class, 'create']);
-    Route::put('/classes/{section}/subjects/{course}', [SectionController::class, 'updateSubject']);
-    Route::put('/students/{student}/enroll', [StudentController::class, 'enroll']);
-    Route::put('/students/{student}', [StudentController::class, 'update']);
-    Route::post('/classes', [SectionController::class, 'create']);
-    Route::patch('/settings/active-department', [SettingsController::class, 'updateActiveDepartment']);
-    Route::post('/settings/school-year', [SchoolYearController::class, 'create']);
-    Route::patch('/settings/school-year', [SchoolYearController::class, 'updateActiveSchoolYear']);
 });

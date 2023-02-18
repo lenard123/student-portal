371312f382752b045f68cc4adca3250ffb0298ca
lenard123 <lenard.mangayayam@gmail.com>
2023-02-18 12:24:12 +0000

    Update admin layouts with sidebar and header changes

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,9 +4,10 @@
 
 @section('content')
 
+@include('layouts.sections.admin-sidebar')
+
 @include('layouts.sections.admin-header')
 
-@include('layouts.sections.admin-sidebar')
 
 <div class="p-4 sm:ml-64 pt-16 min-h-screen flex flex-col relative">
     @yield('admin-content')

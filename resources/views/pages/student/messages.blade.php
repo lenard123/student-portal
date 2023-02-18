034a87c8dbd8f4b2b305c171a731602756f349d3
lenard123 <lenard.mangayayam@gmail.com>
2023-02-18 11:17:39 +0000

    Create MessageController and messages view for student messaging

--- resources/views/pages/student/messages.blade.php
+++ resources/views/pages/student/messages.blade.php
@@ -5,16 +5,32 @@
 @section('student-content')
 
 
-<div class="max-w-lg bg-white mx-auto shadow h-full flex flex-col">
+<div data-app="chat" class="max-w-lg bg-white mx-auto shadow h-full flex flex-col">
     <div class="p-4 text-2xl font-bold text-slate-900 border-b">
         University of Caloocan City
     </div>
 
-    <div class="flex-grow p-4">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Rerum omnis autem nihil exercitationem iste a esse atque cupiditate recusandae facilis unde doloribus, in corporis aperiam, possimus ad. Molestias, consectetur repellendus.
+    <div class="flex-grow overflow-y-auto" ref="body">
+        <div class=" p-4 flex flex-col gap-4">
+            @foreach($messages as $message)
+            @if ($message->receiver_id != null)
+            <div class="bg-slate-50 p-2 px-3 text=slate-800 rounded-lg max-w-[70%] self-start">
+                {{ $message->content }}
+            </div>
+            @else
+            <div class="bg-primary p-2 px-3 text-white rounded-lg max-w-[70%] self-end">
+                {{ $message->content }}
+            </div>
+            @endif
+            @endforeach
+        </div>
     </div>
 
-    <div class="border-t p-4">test</div>
+    <form method="POST" action='{{ url("/messages") }}' class="border-t p-4 flex gap-4">
+        @csrf
+        <fb-input name="content" required placeholder="Enter your message here" class="flex-grow"></fb-input>
+        <fb-button>Send</fb-button>
+    </form>
 
 </div>
 
